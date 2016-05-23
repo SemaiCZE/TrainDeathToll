a85735ea89d5d6bf2608b28c3274e3616081e923
Petr Stefan <ptr.stef@gmail.com>
2016-05-23 17:06:26 +0200

    Regularly get new tweets

--- read_tweets.py
+++ read_tweets.py
@@ -3,7 +3,32 @@
 import twitter
 import json
 import webapp2
-from google.appengine.api import taskqueue
+from google.appengine.api import taskqueue, memcache
+
+
+twitter_user = "cdmimoradnosti"
+queue_name = "new-tweets"
+
+
+def split_list(alist, wanted_parts=1):
+    length = len(alist)
+    return [alist[i*length // wanted_parts: (i+1)*length // wanted_parts]
+            for i in range(wanted_parts)]
+
+
+def get_tweets(twitter_api):
+    cache_tweet_id = 'last_tweet_id'
+
+    last_tweet_id = memcache.get(cache_tweet_id)
+    if last_tweet_id is not None:
+        statuses = twitter_api.GetUserTimeline(screen_name=twitter_user, since_id=last_tweet_id, include_rts=False,
+                                               exclude_replies=True)
+    else:
+        statuses = twitter_api.GetUserTimeline(screen_name=twitter_user, count=200, include_rts=False,
+                                               exclude_replies=True)
+        memcache.add(cache_tweet_id, statuses[0].id, 3600)  # cache for max 1 hour
+
+    return statuses
 
 
 def get_status_info(status):
@@ -55,21 +80,25 @@ class ReadTweets(webapp2.RequestHandler):
         consumer_secret = environ['CONSUMER_SECRET']
         access_key = environ['ACCESS_KEY']
         access_secret = environ['ACCESS_SECRET']
-        twiter_user = "cdmimoradnosti"
-        queue_name = "new-tweets"
 
         q = taskqueue.Queue(queue_name)
         tasks = []
         tweets = []
 
         api = twitter.Api(consumer_key, consumer_secret, access_key, access_secret, cache=None)
-        statuses = api.GetUserTimeline(screen_name=twiter_user)
+        statuses = get_tweets(api)
         for status in statuses:
             status_tag, status_data = get_status_info(status)
             tweets.append(status_data)
             tasks.append(taskqueue.Task(payload=status_data, method='PULL', tag=status_tag))
 
-        q.add(tasks)
+        # Only 100 item at one time can be added to task queue
+        if len(tasks) >= 100:
+            split_tasks = split_list(tasks, wanted_parts=4)
+            for item in split_tasks:
+                q.add(item)
+        else:
+            q.add(tasks)
 
         self.response.out.write('\n'.join(tweets))
 
