383ef3b9f731a2c56cac77a81c9e5ee3f50002f5
Teyras <teyras@gmail.com>
2016-05-29 15:21:26 +0200

    add a basic html template

--- render_page.py
+++ render_page.py
@@ -1,9 +1,18 @@
 import webapp2
+from webapp2_extras import jinja2
 
 
 class MainPage(webapp2.RequestHandler):
+    @webapp2.cached_property
+    def jinja2(self):
+        return jinja2.get_jinja2(app=self.app)
+
+    def render_response(self, template, **context):
+        rv = self.jinja2.render_template(template, **context)
+        self.response.write(rv)
+
     def get(self):
-        self.response.out.write("Main page of TrainDeathToll")
+        self.render_response("stats.html")
 
 
 app = webapp2.WSGIApplication([('/', MainPage), ('/index.html', MainPage)], debug=True)
